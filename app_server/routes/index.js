838b35a52ded41994f1104390755ad9d7b6e3aca
Zeeshan <zeeshan_github@outlook.com>
2018-10-15 09:56:00 +1100

    First: Project Setup

---
+++ app_server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var mainController = require('../controllers/main');
+
+/* GET home page. */
+router.get('/', mainController.index);
+
+
+
+module.exports = router;
